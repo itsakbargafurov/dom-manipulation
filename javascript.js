95948a544ed57d399c34fcff39003820df975b4d
akbar <akbgafurov@gmail.com>
2023-04-04 11:46:36 +0500

    Add heading3 element with blue text using JS and DOM

--- javascript.js
+++ javascript.js
@@ -15,3 +15,11 @@ para.classList.add('content');
 para.textContent = "Hey I'm red!";
 
 container.appendChild(para);
+
+// add a new heading
+const heading3 = document.createElement('h3');
+heading3.style.color = 'blue';
+heading3.classList.add('content');
+heading3.textContent = "I'm a blue h3";
+
+container.appendChild(heading3);
