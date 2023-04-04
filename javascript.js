dcca2c339d54e427d75e7c4710a1d275dcaed2f7
akbar <akbgafurov@gmail.com>
2023-04-04 11:36:33 +0500

    Add paragraph element with red text using JS and DOM

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
+// targets div container with id 'container'
 const container = document.querySelector('#container');
 
+// adds a new div container
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// adds a new paragraph
+const para = document.createElement('p');
+para.style.color = 'red';
+para.classList.add('content');
+para.textContent = "Hey I'm red!";
+
+container.appendChild(para);
